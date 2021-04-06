bf4997813f252f63fe4adea7e1aff300a6ee7779
begulatuk <65555163+begulatuk@users.noreply.github.com>
2021-04-06 23:29:03 +0700

    Update rss.py

--- plugins/rss.py
+++ plugins/rss.py
@@ -190,7 +190,7 @@ async def list_rss_feed(msg: Message):
 async def rss_worker():
     global TASK_RUNNING  # pylint: disable=global-statement
     TASK_RUNNING = True
-    chunk = 20
+    chunk = 2
     if RSS_DICT and RSS_CHAT_ID[0] == Config.LOG_CHANNEL_ID:
         _LOG.info(
             "You have to add var for `RSS_CHAT_ID`, for Now i will send in LOG_CHANNEL")
